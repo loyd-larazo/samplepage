781f465dd9320f7ddb9890edd2deb74249e275a6
Christian Loyd Larazo <30608335+loyd-cpi@users.noreply.github.com>
2020-11-24 10:47:03 +0800

    added index and page2. udpated layout for header and footer

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>This is a sample page</title>
+    <style>
+      body {
+        background: blue;
+      }
+    </style>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  include_once('header.php');
+?>
+
+<h1>This is index page</h1>
+<a href="/page2.php">Go to Page 2</a>
+
+<?php
+  include_once('footer.php');
+?>
